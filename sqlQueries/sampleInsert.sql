a7ea96ab0d7cd25c0ad29dc869a6d8a5a51bd9f7
ctuguina <ctuguina@uw.edu>
2020-10-17 16:37:01 -0700

    edit sample insert and add sample insert csv

--- sqlQueries/sampleInsert.sql
+++ sqlQueries/sampleInsert.sql
@@ -1,2 +1,2 @@
 INSERT INTO SURVEY (SAT, ACT, GPA, Black, White, Hispanic, Asian, Other, InState, Domestic, PrivateSchool, Accepted)
-VALUES (1500, 35, 4.0, 1, 0, 0, 0, 0, 0, 0, 5, 1);
+VALUES (1200, 35, 3.9, 1, 0, 0, 0, 1, 0, 0, 1, 1);
