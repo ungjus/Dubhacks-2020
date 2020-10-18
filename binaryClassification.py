a8d3061d01ec007e4bcc0ac2fc3b01511d2d31cb
ungjus <ungjus19@uw.edu>
2020-10-18 00:48:46 -0700

    pushing

--- binaryClassification.py
+++ binaryClassification.py
@@ -1,5 +1,14 @@
+import json
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+HOST_IP = "localhost"
+HOST_PORT = "4040"
+
+app = Flask(__name__)
+sio = SocketIO(app, cors_allowed_origins="*")
 
 df = pd.read_csv('multipleExampleInsertsCSV.csv')
 
@@ -8,3 +17,9 @@ y = df.iloc[:, 11]
 
 log_model = LogisticRegression().fit(x, y)
 
+@sio.on("Send Form")
+def send_command(form):
+    print(form)
+
+if __name__ == '__main__':
+	sio.run(app, host=HOST_IP, port=HOST_PORT)
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", hello);
+// let display = document.getElementById('display');
 let races = {
    black: 0,
    white: 0,
@@ -7,7 +8,22 @@ let races = {
    other: 0
 };
 function hello() {
-   document.getElementById("studentForm").addEventListener("submit", getStudentInfo);
+   //document.getElementById("studentForm").addEventListener("submit", getStudentInfo);
+   //document.getElementById("predictForm").addEventListener("submit", predict);
+   let student = document.getElementById('studentForm');
+   let predict = document.getElementById('predictForm');
+   if(student){
+      console.log("setting event");
+      student.addEventListener("submit", getStudentInfo);
+   } else {
+      console.log("not setting");
+   }
+   if(predict){
+      console.log("setting event");
+      predict.addEventListener("submit", predict);
+   } else {
+      console.log("not setting");
+   }
 }
 
 function getStudentInfo() {
@@ -23,6 +39,14 @@ function getStudentInfo() {
    insertDatabase(sat, act, gpa, races.black, races.white, races.hispanic,
       races.asian, races.other, inState, domestic, privateSchool, accepted);
    console.log("sent!");
+}
+
+function predict() {
+   console.log("predicting")
+   let display = document.getElementById('display');
+   console.log(display.value);
+   alert("Your chances of getting in is 80%")
+   display.value= "80%";
 }
 
 function toBinary(feature) {
