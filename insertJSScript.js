564c22a5a1968ce72a71771beaba6aae71ea935d
ungjus <ungjus19@uw.edu>
2020-10-17 18:18:41 -0700

    finished main.js

--- insertJSScript.js
+++ insertJSScript.js
@@ -13,27 +13,28 @@ var config =
 const conn = new mysql.createConnection(config);
 
 conn.connect(
-	function (err) { 
-	if (err) { 
-		console.log("!!! Cannot connect !!! Error:");
-		throw err;
-	}
-	else
-	{
-	   console.log("Connection established.");
-           queryDatabase();
-	}	
+	function (err) {
+		if (err) {
+			console.log("!!! Cannot connect !!! Error:" + err.stack);
+		}
+		else
+		{
+		   console.log("Connection established.");
+	           queryDatabase();
+		}
 });
 
 function insertDatabase(sat, act, gpa, black, white, hispanic, asian, other, instate, domestic, privateschool, accepted){
-       conn.query('INSERT INTO SURVEY (SAT, ACT, GPA, Black, White, Hispanic, Asian, Other, InState, Domestic, PrivateSchool, Accepted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', 
-       [sat, act, gpa, black, white, hispanic, asian, other, instate, domestic, privateschool, accepted], 
+       conn.query('INSERT INTO SURVEY (SAT, ACT, GPA, Black, White, Hispanic, Asian, Other, InState, Domestic, PrivateSchool, Accepted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
+       [sat, act, gpa, black, white, hispanic, asian, other, instate, domestic, privateschool, accepted],
       		function (err, results, fields) {
-      			if (err) throw err;
+      			if (err) console.error('error inserting: ' + err.stack);;
 			console.log('Inserted ' + results.affectedRows + ' row(s).');
 	   	})
-	   conn.end(function (err) { 
-		if (err) throw err;
-		else  console.log('Done.') 
+	   conn.end(function (err) {
+		if (err){
+			console.error('error inserting: ' + err.stack);
+			return;
+		} else  console.log('Done.')
 		});
 };
