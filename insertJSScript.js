ab4e076ccbf0b4e885986fa1dfab64bf236440ad
ctuguina <ctuguina@uw.edu>
2020-10-17 19:42:11 -0700

    Update insertJSScript.js

--- insertJSScript.js
+++ insertJSScript.js
@@ -1,38 +1,40 @@
-const mysql = require('mysql');
+const { Connection, Request } = require("tedious");
 
-var config =
-{
-	host: 'mysqlservercmt.database.windows.net',
-	user: 'azureuser',
-	password: 'epicgamer12345.',
-	database: 'mySampleDatabase',
-	port: 3306,
-	ssl: true
-};
-
-const conn = new mysql.createConnection(config);
-
-conn.connect(
-	function (err) { 
-	if (err) { 
-		console.log("!!! Cannot connect !!! Error:");
-		throw err;
+const config = {
+	authentication: {
+	  options: {
+		userName: 'azureuser', // update me
+		password: 'epicgamers12345.' // update me
+	  },
+	  type: 'default'
+	},
+	server: 'mysqlservercmt.database.windows.net', // update me
+	options: {
+	  database: 'mySampleDatabase', //update me
+	  encrypt: true
 	}
-	else
-	{
-	   console.log("Connection established.");
-           queryDatabase();
-	}	
-});
+  };
+  
+  const connection = new Connection(config);
+  
+  // Attempt to connect and execute queries if connection goes through
+  connection.on("connect", err => {
+	if (err) {
+	  console.error(err.message);
+	} else {
+	  console.log("connected");
+	}
+  });
+
 
 function insertDatabase(sat, act, gpa, black, white, hispanic, asian, other, instate, domestic, privateschool, accepted){
-       conn.query('INSERT INTO SURVEY (SAT, ACT, GPA, Black, White, Hispanic, Asian, Other, InState, Domestic, PrivateSchool, Accepted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', 
+       connection.query('INSERT INTO SURVEY (SAT, ACT, GPA, Black, White, Hispanic, Asian, Other, InState, Domestic, PrivateSchool, Accepted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', 
        [sat, act, gpa, black, white, hispanic, asian, other, instate, domestic, privateschool, accepted], 
       		function (err, results, fields) {
       			if (err) throw err;
 			console.log('Inserted ' + results.affectedRows + ' row(s).');
 	   	})
-	   conn.end(function (err) { 
+	   connection.end(function (err) { 
 		if (err) throw err;
 		else  console.log('Done.') 
 		});
